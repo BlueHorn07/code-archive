1c4c218a78f60ff02e289c33b8356a0579ccb26b
bluehorn07 <bluehornblues@gmail.com>
2023-08-03 00:51:07 +0900

    setup and test cognito,

--- nestjs-aws-cognito/src/auth/auth.controller.ts
+++ nestjs-aws-cognito/src/auth/auth.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginRequest, RegisterRequest } from './auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Get()
+  test () {
+    this.authService.register();
+  }
 
   @Post('register')
   async register(
--- nestjs-aws-cognito/src/auth/auth.service.ts
+++ nestjs-aws-cognito/src/auth/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import {
+  CognitoIdentityProviderClient,
+  AdminCreateUserCommand,
+} from '@aws-sdk/client-cognito-identity-provider';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private readonly cognitoClient = new CognitoIdentityProviderClient({
+    region: 'ap-northeast-2',
+  });
+  private readonly USER_POOL_ID = 'ap-northeast-2_RXNM6G6EG';
+
+  async register() {
+    const input = {
+      UserPoolId: this.USER_POOL_ID,
+      Username: 'seokyun_test',
+    };
+
+    const command = new AdminCreateUserCommand(input);
+    const res = await this.cognitoClient.send(command);
+    console.log(res);
+  }
+}
